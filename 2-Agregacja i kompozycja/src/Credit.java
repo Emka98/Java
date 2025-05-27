9d7e47a533c1aa647ce7086ad909ffa893f3c5b0
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-05-27 16:48:08 +0200

    Add files via upload

---
+++ 2-Agregacja i kompozycja/src/Credit.java
@@ -0,0 +1,7 @@
+public class Credit {
+    Person borrower;
+    double cashBorrowed;
+    double cashReturned;
+    double intterestRate;
+    int termMonths;
+}
---
+++ 2-Agregacja i kompozycja/src/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    String firstName;
+    String lastName;
+    String pesel;
+    String street;
+    int houseNumber;
+    int flatNumber;
+    String city;
+    String postkod;
+}
