373b26d8659b68c0335b670175baccc8b57bdd92
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-09-03 10:59:02 +0200

    Add files via upload

---
+++ 01-PrinterTest/src/PrinterTest.java
@@ -0,0 +1,7 @@
+class PrinterTest {
+    public static void main(String[] args) {
+        NumberPrinter np = new NumberPrinter();
+        np.printNumber(5, 10); //call printNumber(int a, int b)
+        np.printNumber(5, 10, 15); //call printNumber(int a, int b, int c)
+    }
+}
