42577e0778973470e94acb013d824b4ad84b02fb
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-09-09 10:15:28 +0200

    Add files via upload

---
+++ 10 - Control instructions/04 - Pointer/src/Point.java
@@ -0,0 +1,46 @@
+class Point {
+
+    int X = 0;
+    int Y = 0;
+
+    public Point (int X, int Y){
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public Point (int X){
+        this.X = X;
+    }
+
+    public Point (int Y){
+        this.Y = Y;
+    }
+
+    public Point (){
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    
+
+
+
+
+
+
+    
+}
