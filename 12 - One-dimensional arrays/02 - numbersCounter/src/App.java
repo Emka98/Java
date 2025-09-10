c7ac0cfd69b363e0a45bd60d4aabe04b005f5a8b
Emka98 <115227079+Emka98@users.noreply.github.com>
2025-09-10 11:27:03 +0200

    Add files via upload

---
+++ 12 - One-dimensional arrays/02 - numbersCounter/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int[] tab1 = {1,2,3};
+        int[] tab2 = {4,5,6};
+
+        SumOfTab sumtab = new SumOfTab();
+
+        System.out.println(sumtab.sumOfTab(tab1));
+        System.out.println(sumtab.sumOfTab(tab2));
+
+        
+        
+        
+        
+        
+    }
+}
